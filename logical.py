d56b16a3f0628fe8e41042ca1ae6f8763062a795
RaunakRaj123 <107362484+RaunakRaj123@users.noreply.github.com>
2022-08-05 09:09:39 +0530

    Add files via upload

---
+++ logical.py
@@ -0,0 +1,10 @@
+a=9
+b=7
+c=10
+if a>b and a>c:
+    print(a)
+elif ( b>c):
+    print(b)
+else:
+    print(c)
+
---
+++ starpattern.py
@@ -0,0 +1,11 @@
+# for x in range(1,5):
+#     for y in range(x):
+#         print(x,end=' ')
+#     x=x+1
+#     print() 
+y=1
+x=5
+for y in range (y,x):
+ print ((x-y)*' '+(2*y-1)*'*')
+
+
